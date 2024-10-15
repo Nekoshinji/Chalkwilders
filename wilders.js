9ac77fe7d9fb5dc85147c4ada6b1710a165cf613
Nekoshinji <obito26@gmail.com>
2024-10-15 11:20:46 +0200

    ajout des fichiers pour avoir 4 wilders en couleurs

---
+++ wilders.js
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log(chalk.blue("Alexandre"));
+console.log(chalk.red("Simeon"));
+console.log(chalk.green.bgYellow.bold("Jules"));
+console.log(chalk.white(chalk.underline.bgBlue("Lise")));
